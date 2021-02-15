d1474deca8cdd38cdca808d70819c1908b05c155
eescriba <ernesto.escriba.pina@gmail.com>
2021-02-15 14:56:58 +0100

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="drl-smartcity",
+    version="0.1.0",
+    description="Deep Reinforcement Learning with Keras-RL and Mesa",
+    author="eescriba",
+    url="https://github.com/eescriba/mesa-keras-rl",
+    package_dir={"": "src/envs/"},
+    packages=[""],
+    install_requires=[
+        "mesa",
+        "jupyter",
+        "numpy",
+        "matplotlib",
+        "keras-rl2",
+        "gym",
+        "h5py",
+        "pillow",
+        "seaborn",
+    ],
+)
